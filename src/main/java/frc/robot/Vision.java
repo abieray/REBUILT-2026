ca7ec2e037dde49a221100f446dfa27c50f02597
abieray <157073219+abieray@users.noreply.github.com>
2026-02-05 19:04:55 -0500

    first commit

---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+
+public class Vision {
+double x=LimelightHelpers.getTX(null); //horizontal offset
+double y=LimelightHelpers.getTY(null); //vertical offset
+double a=LimelightHelpers.getTA(null); //target area (ranges from 0%-100%)
+boolean hasTarget = LimelightHelpers.getTV(null); //makes sure there is a vaild target present
+
+double txnc = LimelightHelpers.getTXNC(null); //horizontal offset from principal pixel/point to target in degrees
+double tync = LimelightHelpers.getTYNC(null); //vertical offset from principal pixel/point to target in degrees
+
+public Vision()
+{
+    LimelightHelpers.setPipelineIndex(null, 0);
+
+}
+
+
+}
+
+
