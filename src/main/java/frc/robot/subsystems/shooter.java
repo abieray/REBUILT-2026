e9f884eb39f40d7933a8e413eeda1728285c59ee
abieray <157073219+abieray@users.noreply.github.com>
2026-02-06 19:13:50 -0500

    added mechanism subsystems

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -1,14 +1,42 @@
 package frc.robot.subsystems;
+import static edu.wpi.first.units.Units.*;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.signals.MotorOutputStatusValue;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//using rev motor for shooter 
-public class shooter extends SubsystemBase
+
+//using talonfx motor for shooter 
+public class Shooter extends SubsystemBase
 {
-final SparkMax spark = new SparkMax(0, MotorType.kBrushless); //placeholder id for now
+  
+   final TalonFX m_shooter_motor = new TalonFX(0); //placeholder id 
+   final CANcoder m_shooter_CaNcoder = new CANcoder(1); //placeholder id
 
+   public Shooter(){
+    
 
+   }
+  
+
+   
 }
+        
+
+
+
+     
+    
+
+
+
